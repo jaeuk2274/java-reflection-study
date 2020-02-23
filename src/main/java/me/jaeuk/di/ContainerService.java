20461e144fec6ae4749a2c8074afd11e309d7111
jaeuk2274 <jaeuk2274@gmail.com>
2020-02-23 19:55:13 +0900

    di framework study

--- src/main/java/me/jaeuk/di/ContainerService.java
+++ src/main/java/me/jaeuk/di/ContainerService.java
@@ -4,6 +4,8 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 
 // di framework 를 직접 만들보면서 스터디.
+// 싱글톤 스콥도 아니고, 캐싱도 없고 등. 스프링 IoC 컨테이너와 비교하면 많이 모자라지만
+// reflection 로 이런 것도 구현을 하는구나 스터디 용도
 public class ContainerService {
 
     public static <T> T getObject(Class<T> classType)  {
